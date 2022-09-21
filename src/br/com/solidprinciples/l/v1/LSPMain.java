e6a6f6f69f2b40fa1388e08eee55d11abc741176
Regis <carlos.filipe.regis@accenture.com>
2022-09-21 11:51:41 -0300

    ajustes nos objetos

--- src/br/com/solidprinciples/l/v1/LSPMain.java
+++ src/br/com/solidprinciples/l/v1/LSPMain.java
@@ -4,8 +4,9 @@ public class LSPMain {
 
 	public static void main(String[] args) {
 		TrasportationDevice b =  new Bicycle();
+		b.setName("bike");
 		TrasportationDevice c =  new Car();
-		
+		c.setName("car");
 		System.out.println(b.getName());
 		System.out.println(b.getSpeed());
 		
--- src/br/com/solidprinciples/o/v3/InsuranceSurveyor.java
+++ src/br/com/solidprinciples/o/v3/InsuranceSurveyor.java
@@ -2,4 +2,5 @@ package br.com.solidprinciples.o.v3;
 
 public abstract class InsuranceSurveyor {
 	public abstract boolean isValidClaim();
+	
 }
